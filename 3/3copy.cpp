56d90b9bd9fba74ca93fee373126f7da6af71b13
alt-nikitha <nikithasrikanth1711@gmail.com>
2021-12-08 09:42:20 +0530

    added new parallel

--- 3/3copy.cpp
+++ 3/3copy.cpp
@@ -13,6 +13,7 @@ long unfriendly(long n){
     long m = (long)sqrt((double)n);
     long count = 0;
     bool *composite = new bool[n+1];
+    double t = omp_get_wtime();
     for(long i=2;i<=m;i++){
         if(!composite[i]){
             count++;
@@ -22,6 +23,7 @@ long unfriendly(long n){
     for(long i=m+1;i<=n;i++){
         if(!composite[i]) count++;
     }
+    printf("time unfriendly  : %lf\n", omp_get_wtime()-t);
     return count;
 }
 long friendly(long n){
@@ -32,6 +34,7 @@ long friendly(long n){
     memset(composite,0,n);
     long count = 0;
     long n_factor = 0;
+    double t = omp_get_wtime();
     for(long i=2;i<=m;i++){
         if(!composite[i]){
             ++count;
@@ -48,15 +51,54 @@ long friendly(long n){
             if(!composite[i]) count++;
         }
     }
+    printf("time friendly : %lf\n", omp_get_wtime()-t);
+    return count;
+
+}
+
+long friendly_parallel(long n){
+    long m = (long)sqrt((double)n);
+    long *factor = new long[m];
+    long *striker = new long[m];
+    bool *composite = new bool[n+1];
+    memset(composite,0,n);
+    long count = 0;
+    long n_factor = 0;
+    double t = omp_get_wtime();
+    #pragma omp single
+    {
+        for(long i=2;i<=m;i++){
+        if(!composite[i]){
+            ++count;
+            striker[n_factor] = Striker(composite, 2*i, i,m);
+            factor[n_factor++] = i;
+        }
+    }
+    }
+    
+    #pragma omp parallel for reduction(+:count) shared(composite, striker, factor)
+    for(long window=m+1;window<=n;window+=m){
+        long limit = min(window+m-1,n);
+        for(long k = 0; k<n_factor;k++){
+            striker[k] = Striker(composite,striker[k], factor[k], limit);
+        }
+        for(long i = window; i<=limit;i++){
+            if(!composite[i]) count++;
+        }
+    }
+
+    printf("time friendly parallel : %lf\n", omp_get_wtime()-t);
 
     return count;
 
 }
+
 long parallel_sieve(long n){
     long m = (long)sqrt((double)n);
     long count =0;
     long n_factor = 0;
     long *factor = new long[n];
+    double t = omp_get_wtime();
     #pragma omp parallel
     {
         bool *composite = new bool[m+1];
@@ -93,6 +135,7 @@ long parallel_sieve(long n){
                 base+=m;
             }
     }
+    printf("time parallel : %lf\n", omp_get_wtime()-t);
     return count;
 }
 int main(int argc, char **argv){
@@ -104,6 +147,8 @@ int main(int argc, char **argv){
     count = friendly(n);
     cout<<count<<endl;
     count = unfriendly(n);
+    cout<<count<<endl;
+    count = friendly_parallel(n);
     cout<<count<<endl;
 
 
